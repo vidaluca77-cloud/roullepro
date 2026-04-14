160060d07a374362fd2f9807dea7962a50ed2793
La Vida Luca <vidaluca77@gmail.com>
2026-04-14 16:09:09 +0200

    Add Next.js configuration for remote images

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ypgolzcibtjljfydxcun.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
