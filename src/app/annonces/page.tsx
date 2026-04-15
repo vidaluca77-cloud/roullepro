50b4ebd296b75bd1539a217cbbdaa9f91a6b6e4a
La Vida Luca <vidaluca77@gmail.com>
2026-04-15 15:10:43 +0200

    Wrap AnnonceCard with Link for navigation

--- src/app/annonces/page.tsx
+++ src/app/annonces/page.tsx
@@ -82,4 +82,6 @@ export default function AnnoncesPage() {
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
             {filtered.map((a) => (
-              <L              <AnnonceCard key={a.id} annonce={a} />
+            <Link href={`/annonces/${a.id}`}>
+              <AnnonceCard key={a.id} annonce={a} />
+            </Link>
